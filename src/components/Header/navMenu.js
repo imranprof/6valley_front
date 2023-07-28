f3dffcd091818e4a46b59c3d688cd9fdd8bb43b9
imranprof <imranpers@gmail.com>
2023-07-28 11:52:38 +0600

    feat: add main nav menu

--- src/components/Header/navMenu.js
+++ src/components/Header/navMenu.js
@@ -1,10 +1,27 @@
 import styles from '../../styles/navMenu.module.css'
+import {AiOutlineMenu} from 'react-icons/ai'
+import {IoIosArrowDown} from 'react-icons/io'
 
 export const NavMenu = () => {
   
     return (
 		<div className={styles.navMenu}>
-            <h2>This is main menu</h2>
+      <div className={styles.category}>
+        <AiOutlineMenu className={styles.icon} />
+        Browse Category
+        <IoIosArrowDown className={styles.arIcon}/>
+        </div>
+      <div className={styles.menuItem}>Home</div>
+      <div className={styles.menuItem}>Offers
+        <IoIosArrowDown />
+      </div>
+      <div className={styles.menuItem}>Stores
+        <IoIosArrowDown />
+      </div>
+      <div className={styles.menuItem}>Brands
+        <IoIosArrowDown />
+      </div>
+      <div className={styles.menuItem}>Discounted Products</div>
 		</div>
     );
   };
